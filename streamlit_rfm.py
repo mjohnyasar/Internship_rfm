08796252c9d8226fc7f4bff8b10d2d136676d3e7
M. John Yasar <m.john.yasar@gmail.com>
2022-10-07 02:23:14 -0700

    Add files via upload

--- streamlit_rfm.py
+++ streamlit_rfm.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-data_rfm = pd.read_excel("rfm.xlsx") 
+data_rfm = pd.read_excel("/content/drive/MyDrive/ColabNotebooks/Internship/Group2/RFM/rfm.xlsx") 
 df=data_rfm.copy().set_index("customer_no")
 len_df=len(df)
 
@@ -12,10 +12,11 @@ len_df=len(df)
 st.header(" CUSTOMER RFM POINT INSPECTION")
 st.subheader("(Data: Coworce )")
 
-#st.image("rfm_metrics.png",width=600, caption="It is made of 3 main criterias?")
-#st.image("champions.png",width=600,caption="Explore the groups ")
+#st.image("/content/drive/MyDrive/ColabNotebooks/Internship/Group2/RFM/rfm_metrics.png",width=600, caption="It is made of 3 main criterias?")
+#st.image("/content/drive/MyDrive/ColabNotebooks/Internship/Group2/RFM/champions.png",width=600,caption="Explore the groups ")
 st.image("https://d35fo82fjcw0y8.cloudfront.net/2018/03/01013508/Incontent_image.png",width=600,caption="It is made of 3 main criterias? ")
 st.image("https://d35fo82fjcw0y8.cloudfront.net/2018/03/01013239/Header-e1551869702205.png",width=600,caption="Explore the groups ")
+
 
 
 st.write("## CUSTOMER RFM POINTS")
@@ -29,8 +30,8 @@ st.info(" Total Number of Customers  : {}".format(len_df))
 
 label1="first 2 digit"
 label2="second 2 digit"
-a=st.slider(label1, min_value=1, max_value=28, value=17,step=1, label_visibility="visible");
-b=st.slider(label2, min_value=0, max_value=19, value=05,step=1, label_visibility="visible")
+a=st.slider(label1, min_value=1, max_value=28, value=14,step=1, label_visibility="visible");
+b=st.slider(label2, min_value=0, max_value=19, value=10,step=1, label_visibility="visible")
 c=a*100+b
 st.write("CHOSEN CUSTOMER NO :", c)
 
@@ -38,6 +39,14 @@ st.write("CHOSEN CUSTOMER NO :", c)
 dff=df.iloc[c-1]
 st.table(dff)
 st.write(dff)
+
+dff=df[["Recency","Frequency","Monetary"]]
+dff.hist()
+
+sns.histplot(data=df[["Recency"]])
+sns.histplot(data=df[["Frequency"]])
+sns.histplot(data=df[["Monetary"]])
+
 
 #________________________________
 numeric=df.select_dtypes(include=['int64','float64']).columns
@@ -49,8 +58,8 @@ def plot_hist(f1,feature):
     plt.title("{} distribution with hist".format(feature))
     plt.show()
     st.write(fig)      # st.pyplot(fig) 
-for n in numeric:
-    plot_hist(df,n)
+#for n in numeric:
+#   plot_hist(df,n)
 #______________________________________
 
 def hist_plot(f1, feature):
@@ -72,8 +81,10 @@ def countPlot(f1,feature):
     fig = plt.figure(figsize=(10, 4))
     sns.countplot(x = feature, data = f1)
     st.pyplot(fig)
-for n in objects:
-    countPlot(df,n)
+# for n in objects:
+#    countPlot(df,n)
+
+
 
 
 #------------------------------------------
