0624de0f41fa45e8af3a1c4537a9752a7e4c891f
M. John Yasar <m.john.yasar@gmail.com>
2022-10-15 18:17:42 -0700

    Update streamlit_rfm.py

--- streamlit_rfm.py
+++ streamlit_rfm.py
@@ -5,7 +5,8 @@ import seaborn as sns
 #import matplotlib.pyplot as plt
 
 data_rfm = pd.read_csv("rfm.csv") 
-df=data_rfm.copy().set_index("customer_no")
+df=data_rfm.copy()
+#df=data_rfm.copy().set_index("customer_no")
 len_df=len(df)
 
 
@@ -23,6 +24,31 @@ st.write("## CUSTOMER RFM POINTS")
 st.write()
 st.write()
 
+#======================================================================
+#st.set_page_config(page_title ="RFM Analysis",page_icon= ":bar_chart:", layout= "wide")
+
+df["customer_id"]=df["customer_id"].astype(str)
+st.header("All Data:")
+st.dataframe(df)
+
+    
+st.sidebar.subheader("Please type the Customer ID")
+customers=df["customer_id"].drop_duplicates()
+customer_id= st.sidebar.selectbox("Select the Customer",customers)
+
+st.sidebar.subheader("Please select Segments")
+segments=df["Segment"].drop_duplicates()
+segments= st.sidebar.selectbox("Select Segments",segments)
+
+
+df_selection = df.query(
+    "customer_id == @customer_id")
+
+st.subheader("Results of Customer Filter:")
+st.write(df.loc[(df['customer_id']==customer_id)])
+st.subheader("Results of Segment Filter:")
+st.write(df.loc[(df['Segment']==segments)])
+*________________________________________________
 
 st.subheader(" Please choose the customer_no ")
 
